b48df4b77330f8f4e0f3d10bd2caad84432cc215
JhonierMejia10 <jhonimejiajaramillo@gmail.com>
2025-10-08 11:17:31 -0500

    Se registraron los modelos necesarios en los correspondientes archivos admin para interactuar con los modelos desde el panel de administración

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Orden,OrdenItem,Carrito
 
 # Register your models here.
+admin.site.register(OrdenItem)
+admin.site.register(Orden)
+admin.site.register(Carrito)
