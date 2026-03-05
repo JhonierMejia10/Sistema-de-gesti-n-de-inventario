1ce2f4934bf8abb51a68510d599a0de431bf5757
Jhonier Mejía <124406476+JhonierMejia10@users.noreply.github.com>
2026-03-04 20:22:09 -0500

    Delete almacenes directory

--- almacenes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Ubicacion,Almacen
-
-# Register your models here.
-admin.site.site_header = "Adminisración de almacenes"
-admin.site.register(Ubicacion)
-admin.site.register(Almacen)
